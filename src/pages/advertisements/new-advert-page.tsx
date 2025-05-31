37e58ee15d780a6c8f1c428d0412c04fcb38634b
César Redondo Paulino <cesarlmf8@gmail.com>
2025-05-31 15:47:42 +0200

    Feat: Creando la pagina de nuevo anuncio

---
+++ src/pages/advertisements/new-advert-page.tsx
@@ -0,0 +1,97 @@
+
+
+
+export const NewAdvertPage = () => {
+    return (
+        <div>
+            <h1>
+                Crear anuncio
+            </h1>
+            <form
+            onSubmit={handleSubmit}
+            >
+            {/* <Notification
+                successMessage={successMessage}
+                errorMessage={errorMessage}
+            /> */}
+            <div>
+                <label 
+                htmlFor="name"
+                >
+                    Nombre <span className="requiered">*</span>
+                </label>
+                <input 
+                type="text"
+                name="name"
+                id="name"
+                required
+                value={FormData.name}
+                onChange={handlechange} 
+                />
+            </div>
+            
+            <div>
+                <label 
+                htmlFor="name"
+                >
+                    Precio <span className="requiered">*</span>
+                </label>
+                <input 
+                type="number"
+                name="price"
+                id="price"
+                required
+                value={FormData.price}
+                onChange={handlechange} 
+                />
+            </div>
+
+            <div>
+            <label>
+                Tags <span className="requiered">*</span>
+            </label>
+                <div>
+                    {tags.map((tag) => (
+                        <label key={tag}>
+                            <input type="checkbox"
+                            name="tags"
+                            value={tag}
+                            className="peer hidden"
+                            checked={selectedtags.includes(tag)}
+                            onChange={() => toggleTags(tag)} 
+                            />
+                        <span>
+                            {tag}
+                        </span>
+                        </label>
+                    ))}
+                </div>
+            </div>
+
+            <div>
+                <label htmlFor="">
+                    Tipo <span className="requiered">*</span>
+                </label>
+                <div>
+                    <label 
+                    htmlFor="compra"
+                    >
+                        <input 
+                        type="radio"
+                        name="sale"
+                        id="compra"
+                        value="compra"
+                        required
+                        checked={FormData.sale === true}
+                        onChange={handleChange}
+                        />
+                        <span>
+                            venta
+                        </span>
+                    </label>
+                </div>
+            </div>
+            </form>
+        </div>
+    )
+}
